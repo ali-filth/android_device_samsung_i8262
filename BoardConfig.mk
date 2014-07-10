6df8ca2f2286471c0f0a46d8c40be92d14212ab5
ali-filth <ali.filthx@gmail.com>
2014-07-11 05:28:32 +0800

    stable cwm recovery

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,30 +1,53 @@
 USE_CAMERA_STUB := true
 
-# inherit from the proprietary version
+## inherit from the proprietary version
 -include vendor/samsung/i8262/BoardConfigVendor.mk
 
 TARGET_ARCH := arm
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
+TARGET_BOARD_PLATFORM := msm7x27a
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := cortex-a5
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_BOOTLOADER_BOARD_NAME := i8262
 
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+## Kernel
+TARGET_PREBUILT_KERNEL := device/samsung/i8262/kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom loglevel=1 vmalloc=200M
 BOARD_KERNEL_BASE := 0x00200000
 BOARD_KERNEL_PAGESIZE := 4096
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
+## fix this up by examining /proc/mtd on a running device
+BOARD_BOOTIMAGE_PARTITION_SIZE := 12582912
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 12582912
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1004535296
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1291845120
 BOARD_FLASH_BLOCK_SIZE := 131072
 
-TARGET_PREBUILT_KERNEL := device/samsung/i8262/kernel
 
+## Recovery
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_DOWNLOAD_MODE := true
+TARGET_RECOVERY_FSTAB := device/samsung/i8262/ramdisk/root/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+#TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+#BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/i8262/recovery/graphics.c
+#BOARD_RECOVERY_SWIPE := true
+
+## SE Policy
+BOARD_SEPOLICY_DIRS += device/samsung/i8262/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+	file_contexts
+
+## Samsung has weird framebuffer
+TARGET_NO_INITLOGO := true
--- device_i8262.mk
+++ device_i8262.mk
@@ -17,6 +17,22 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
+# Ramdisk
+PRODUCT_PACKAGES += \
+	fstab.qcom \
+	charger \
+	charger_res_images
+
+# Philz
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/extra.fstab:recovery/root/etc/extra.fstab \
+    $(LOCAL_PATH)/recovery/initlogo-cwm.rle:recovery/root/initlogo.rle \
+    $(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
+
+# TWRP
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/twrp.fstab
+
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
